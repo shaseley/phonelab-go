fb75b3fd234f539520f84a6c03792807b64e143e
Scott Haseley <shaseley@buffalo.edu>
2016-12-14 21:57:39 -0500

    Converted printk

--- parse_printk.go
+++ parse_printk.go
@@ -25,6 +25,15 @@ const (
 	PM_SUSPEND_EXIT
 )
 
+/* Printk pattern example: <6>[   21.512807] msm_thermal: Allow Online CPU3 Temp: 66 */
+var PRINTK_PATTERN_STRING = `(<(?P<loglevel>\d+)>)?\s*\[\s*(?P<timestamp>\d+\.\d+)\]\s*`
+
+///////////////////////////////////////////////////////////////////////////////
+// MSM thermal printk messages (temperature)
+
+var MSM_THERMAL_PRINTK_PATTERN = regexp.MustCompile(PRINTK_PATTERN_STRING +
+	`\s*msm_thermal: (?P<state>(Set Offline:|Allow Online)) CPU(?P<cpu>\d+) Temp: (?P<temp>\d+)`)
+
 type MsmThermalPrintk struct {
 	Logline  *Logline        `logcat:"-"`
 	StateStr string          `logcat:"state"`
@@ -33,61 +42,61 @@ type MsmThermalPrintk struct {
 	Temp     int             `logcat:"temp"`
 }
 
-/* Printk pattern example: <6>[   21.512807] msm_thermal: Allow Online CPU3 Temp: 66 */
-var PRINTK_PATTERN_STRING = `(<(?P<loglevel>\d+)>)?\s*\[\s*(?P<timestamp>\d+\.\d+)\]\s*`
-var MSM_THERMAL_PRINTK_PATTERN = regexp.MustCompile(PRINTK_PATTERN_STRING +
-	`\s*msm_thermal: (?P<state>(Set Offline:|Allow Online)) CPU(?P<cpu>\d+) Temp: (?P<temp>\d+)`)
+type MsmThermalParser struct {
+	RegexParser *RegexParser
+}
 
-func ParseMsmThermalPrintk(logline *Logline) *MsmThermalPrintk {
-	var err error
+func NewMsmThermalParser() *MsmThermalParser {
+	parser := &MsmThermalParser{}
+	parser.RegexParser = NewRegexParser(parser)
+	return parser
+}
 
-	names := MSM_THERMAL_PRINTK_PATTERN.SubexpNames()
-	values_raw := MSM_THERMAL_PRINTK_PATTERN.FindAllStringSubmatch(logline.Payload, -1)
-	if values_raw == nil {
-		//fmt.Fprintln(os.Stderr, "Failed to parse:", logline.Payload)
-		return nil
+func (p *MsmThermalParser) Parse(line string) (interface{}, error) {
+	var mtp *MsmThermalPrintk
+
+	if obj, err := p.RegexParser.Parse(line); err != nil {
+		return obj, err
+	} else {
+		mtp = obj.(*MsmThermalPrintk)
 	}
 
-	values := values_raw[0]
+	// TODO: fix
+	// mtp.Logline = logline
 
-	kv_map := map[string]string{}
-
-	for i, value := range values {
-		kv_map[names[i]] = value
-	}
-
-	mtp := new(MsmThermalPrintk)
-
-	mtp.Logline = logline
-	mtp.StateStr = kv_map["state"]
+	// Set state
 	if strings.Compare(mtp.StateStr, "Set Offline:") == 0 {
 		mtp.State = MSM_THERMAL_STATE_OFFLINE
 	} else if strings.Compare(mtp.StateStr, "Allow Online") == 0 {
 		mtp.State = MSM_THERMAL_STATE_ONLINE
 	} else {
-		fmt.Fprintln(os.Stderr, fmt.Sprintf("Unknown msm_thermal state '%s'", mtp.StateStr))
-		os.Exit(-1)
+		return nil, fmt.Errorf("Unknown msm_thermal state '%s'", mtp.StateStr)
 	}
-	var cpu int64
-	var temp int64
-	if cpu, err = strconv.ParseInt(kv_map["cpu"], 0, 32); err != nil {
-		return nil
-	}
-	mtp.Cpu = int(cpu)
 
-	if temp, err = strconv.ParseInt(kv_map["temp"], 0, 32); err != nil {
-		return nil
-	}
-	mtp.Temp = int(temp)
-
-	return mtp
+	return mtp, nil
 }
 
-type PowerManagementPrintk struct {
-	Logline  *Logline
-	State    PowerManagementState
-	Datetime time.Time
+func (p *MsmThermalParser) New() interface{} {
+	return &MsmThermalPrintk{}
 }
+
+func (p *MsmThermalParser) Regex() *regexp.Regexp {
+	return MSM_THERMAL_PRINTK_PATTERN
+}
+
+func ParseMsmThermalPrintk(logline *Logline) *MsmThermalPrintk {
+	parser := NewMsmThermalParser()
+	if obj, err := parser.Parse(logline.Payload); err != nil {
+		return nil
+	} else {
+		mtp := obj.(*MsmThermalPrintk)
+		mtp.Logline = logline
+		return mtp
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// PowerManger Suspend entry and exit
 
 /* Format:
 <6>[93341.687692] PM: suspend exit 2016-04-27 04:00:00.220795560 UTC
@@ -97,106 +106,130 @@ type PowerManagementPrintk struct {
 var POWER_MANAGEMENT_PRINTK_PATTERN = regexp.MustCompile(PRINTK_PATTERN_STRING +
 	`\s*PM: suspend (?P<state>entry|exit) (?P<datetime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+) (?P<timezone>\S+)`)
 
-func ParsePowerManagementPrintk(logline *Logline) *PowerManagementPrintk {
-	var err error
+type PowerManagementPrintk struct {
+	Logline  *Logline             `logcat:"-"`
+	State    PowerManagementState `logcat:"-"`
+	Datetime time.Time            `logcat:"-"`
+}
 
-	names := POWER_MANAGEMENT_PRINTK_PATTERN.SubexpNames()
-	values_raw := POWER_MANAGEMENT_PRINTK_PATTERN.FindAllStringSubmatch(logline.Payload, -1)
-	if values_raw == nil {
-		//fmt.Fprintln(os.Stderr, "Failed to parse:", logline.Payload)
-		return nil
+type PMManagementParser struct {
+	RegexParser *RegexParser
+}
+
+func NewPMManagementParser() *PMManagementParser {
+	parser := &PMManagementParser{}
+	parser.RegexParser = NewRegexParser(parser)
+	return parser
+}
+
+func (p *PMManagementParser) New() interface{} {
+	return &PowerManagementPrintk{}
+}
+
+func (p *PMManagementParser) Regex() *regexp.Regexp {
+	return POWER_MANAGEMENT_PRINTK_PATTERN
+}
+
+func (p *PMManagementParser) Parse(line string) (interface{}, error) {
+	var pmp *PowerManagementPrintk
+
+	if obj, err := p.RegexParser.Parse(line); err != nil {
+		return obj, err
+	} else {
+		pmp = obj.(*PowerManagementPrintk)
 	}
 
-	values := values_raw[0]
+	m := p.RegexParser.LastMap
 
-	kv_map := map[string]string{}
+	// TODO: Fix
+	//pmp.Logline = logline
 
-	for i, value := range values {
-		kv_map[names[i]] = value
-	}
-	pmp := new(PowerManagementPrintk)
-
-	pmp.Logline = logline
-
-	if strings.Compare(kv_map["state"], "entry") == 0 {
+	if strings.Compare(m["state"], "entry") == 0 {
 		pmp.State = PM_SUSPEND_ENTRY
-	} else if strings.Compare(kv_map["state"], "exit") == 0 {
+	} else if strings.Compare(m["state"], "exit") == 0 {
 		pmp.State = PM_SUSPEND_EXIT
 	} else {
-		fmt.Fprintln(os.Stderr, "Unknown pm state for line:", logline.Line)
-		os.Exit(-1)
+		return nil, fmt.Errorf("Unknown pm state for line: %v", line)
 	}
 
-	datetime, err := strptime.Parse(kv_map["datetime"], "%Y-%m-%d %H:%M:%S.%f")
-	if err != nil {
-		return nil
+	if datetime, err := strptime.Parse(m["datetime"], "%Y-%m-%d %H:%M:%S.%f"); err != nil {
+		return nil, err
+	} else {
+		pmp.Datetime = datetime
 	}
-	pmp.Datetime = datetime
-	return pmp
+
+	return pmp, nil
 }
+
+func ParsePowerManagementPrintk(logline *Logline) *PowerManagementPrintk {
+	parser := NewPMManagementParser()
+	if obj, err := parser.Parse(logline.Payload); err != nil {
+		return nil
+	} else {
+		pmp := obj.(*PowerManagementPrintk)
+		pmp.Logline = logline
+		return pmp
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Healthd logs (battery stats)
 
 var HEALTHD_PATTERN = regexp.MustCompile(`` +
 	PRINTK_PATTERN_STRING +
 	`healthd:\s*battery\s*l=(?P<l>\d+)\s+v=(?P<v>\d+)\s+t=(?P<t>\d+\.\d+)\s+h=(?P<h>-?\d+)\s+st=(?P<st>-?\d+)\s+c=(?P<c>-?\d+)\s+chg=(?P<chg>([auw]+)?)\s*`)
 
 type Healthd struct {
-	Logline   *Logline
-	Timestamp float64
-	L         int
-	V         int
-	T         float64
-	H         int
-	St        int
-	C         int
-	Chg       string
+	Logline   *Logline `logcat:"-"`
+	Timestamp float64  `logcat:"timestamp"`
+	L         int      `logcat:"l"`
+	V         int      `logcat:"v"`
+	T         float64  `logcat:"t"`
+	H         int      `logcat:"h"`
+	St        int      `logcat:"st"`
+	C         int      `logcat:"c"`
+	Chg       string   `logcat:"chg"`
 }
 
+type HealthdParser struct {
+	RegexParser *RegexParser
+}
+
+func NewHealthdParser() *HealthdParser {
+	parser := &HealthdParser{}
+	parser.RegexParser = NewRegexParser(parser)
+	return parser
+}
+
+func (p *HealthdParser) New() interface{} {
+	return &MsmThermalPrintk{}
+}
+
+func (p *HealthdParser) Regex() *regexp.Regexp {
+	return HEALTHD_PATTERN
+}
+
+func (p *HealthdParser) Parse(line string) (interface{}, error) {
+	// Currently, this just wraps the regex parser
+	// TODO: Fix
+	//pmp.Logline = logline
+	return p.RegexParser.Parse(line)
+}
+
+// Legacy
 func ParseHealthdPrintk(logline *Logline) *Healthd {
-	names := HEALTHD_PATTERN.SubexpNames()
-	values_raw := HEALTHD_PATTERN.FindAllStringSubmatch(logline.Payload, -1)
-	if len(values_raw) == 0 {
-		fmt.Fprintln(os.Stderr, "Failed to parse:", logline.Line)
-		os.Exit(-1)
-	}
-	values := values_raw[0]
-
-	kv_map := map[string]string{}
-	for i, value := range values {
-		kv_map[names[i]] = value
-	}
-	timestamp, err := strconv.ParseFloat(kv_map["timestamp"], 64)
-	if err != nil {
+	parser := NewHealthdParser()
+	if obj, err := parser.Parse(logline.Payload); err != nil {
 		return nil
+	} else {
+		hd := obj.(*Healthd)
+		hd.Logline = logline
+		return hd
 	}
-	l, err := strconv.ParseInt(kv_map["l"], 0, 32)
-	if err != nil {
-		return nil
-	}
-	v, err := strconv.ParseInt(kv_map["v"], 0, 32)
-	if err != nil {
-		return nil
-	}
-	t, err := strconv.ParseFloat(kv_map["t"], 64)
-	if err != nil {
-		return nil
-	}
-	h, err := strconv.ParseInt(kv_map["h"], 0, 32)
-	if err != nil {
-		return nil
-	}
-	st, err := strconv.ParseInt(kv_map["st"], 0, 32)
-	if err != nil {
-		return nil
-	}
-	c, err := strconv.ParseInt(kv_map["c"], 0, 32)
-	if err != nil {
-		return nil
-	}
-	chg := kv_map["chg"]
-
-	obj := Healthd{logline, timestamp, int(l), int(v), t, int(h), int(st), int(c), chg}
-	return &obj
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// Healthd logs (battery stats)
 
 var PVS_BIN_PATTERN = regexp.MustCompile(`` +
 	PRINTK_PATTERN_STRING +
