5ea0ee9b6d9b33a529fb27504f2df0cb8429e96b
Scott Haseley <shaseley@buffalo.edu>
2017-04-13 21:30:58 -0400

    Initial server commit

---
+++ phonelab-go-server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+)
+
+func initCommands() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:   "phoneLab-go-server",
+		Short: "PhoneLab-Go! Job Server",
+		Run:   serverCmdRun,
+	}
+
+	serverCmdInitFlags(rootCmd)
+
+	return rootCmd
+}
+
+func fatalError(err error) {
+	fmt.Fprintf(os.Stderr, "Error processing command: %v\n", err)
+	os.Exit(1)
+}
+
+// Modified from http://nathanleclaire.com/blog/2014/08/24/handling-ctrl-c-interrupt-signal-in-golang-programs/
+func waitForSignal() {
+	signalChan := make(chan os.Signal, 1)
+	doneChan := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
+	signal.Notify(signalChan, os.Kill)
+
+	go func() {
+		for _ = range signalChan {
+			fmt.Println("Killing...")
+			if server != nil {
+				server.Stop()
+			}
+			doneChan <- true
+		}
+	}()
+
+	<-doneChan
+}
+
+func main() {
+
+	mainCmd := initCommands()
+
+	go func() {
+		if err := mainCmd.Execute(); err != nil {
+			os.Exit(-1)
+		}
+	}()
+
+	waitForSignal()
+}
---
+++ phonelab-go-server/server.go
@@ -0,0 +1,319 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/kr/beanstalk"
+	phonelab "github.com/shaseley/phonelab-go"
+	"github.com/spf13/cobra"
+	yaml "gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"sync"
+	"time"
+)
+
+var logger = log.New(os.Stderr, "phonelab-go-server", log.LstdFlags)
+
+// Details about the non-split experiment
+type metaJob struct {
+	Id   int64
+	Dir  string
+	User string
+	Name string
+}
+
+func (job *metaJob) tubeName() string {
+	return fmt.Sprintf("%v_%v", job.User, job.Name)
+}
+
+type SubmissionServer struct {
+	Port            int
+	BeanstalkPort   int
+	BeanstalkServer string
+	JobDir          string
+
+	conn      *beanstalk.Conn
+	isTempDir bool
+	nextId    int64
+
+	sync.Mutex
+}
+
+var server *SubmissionServer
+
+func NewSubmissionServer(port, beanstalkPort int, jobDir, beanstalkServer string) *SubmissionServer {
+	return &SubmissionServer{
+		Port:            port,
+		BeanstalkPort:   beanstalkPort,
+		BeanstalkServer: beanstalkServer,
+		JobDir:          jobDir,
+		nextId:          1,
+	}
+}
+
+var (
+	submitConfPort          int
+	submitConfBeanstalkPort int
+	submitConfJobDir        string
+)
+
+func serverCmdInitFlags(cmd *cobra.Command) {
+	cmd.Flags().IntVarP(&submitConfPort, "port", "p", 8000, "The job server port to listen on")
+	cmd.Flags().IntVarP(&submitConfBeanstalkPort, "beanstalk", "b", 14000, "The beanstalkd port to use for jobs")
+	cmd.Flags().StringVarP(&submitConfJobDir, "jobdir", "j", "", "The directory to store job details. By default, this is stored in /tmp")
+}
+
+// Entry point for phonelab-go-server
+func serverCmdRun(cmd *cobra.Command, args []string) {
+	server = NewSubmissionServer(submitConfPort, submitConfBeanstalkPort, submitConfJobDir, "localhost")
+	server.Start()
+}
+
+// Kick off the server
+func (s *SubmissionServer) Start() {
+	// Init
+	if len(s.JobDir) == 0 {
+		var err error
+		s.JobDir, err = ioutil.TempDir("", "phonelab-go")
+		if err != nil {
+			panic(err)
+		}
+		s.isTempDir = true
+	}
+
+	if c, err := beanstalk.Dial("tcp", fmt.Sprintf("%v:%v", s.BeanstalkServer, s.BeanstalkPort)); err != nil {
+		panic(fmt.Sprintf("Unable to connect to beanstalk: %v", err))
+	} else {
+		s.conn = c
+	}
+
+	// Run
+	logger.Printf("Starting PhoneLab-Go! server on port %v\n", s.Port)
+	logger.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", s.Port), NewRouter()))
+}
+
+// Stop is called when we're being killed. Clean up and free any resources.
+func (s *SubmissionServer) Stop() {
+	if s.isTempDir {
+		os.RemoveAll(s.JobDir)
+	}
+	s.conn.Close()
+}
+
+// Find the next available id and create the directory for the files.
+func (s *SubmissionServer) prepareJob(user, name string) (*metaJob, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	for {
+		id := s.nextId
+		s.nextId += 1
+		d := path.Join(s.JobDir, fmt.Sprintf("%v", id))
+
+		if _, err := os.Stat(d); err == nil {
+			continue
+		}
+
+		if err := os.Mkdir(d, 0775); err != nil {
+			return nil, err
+		}
+
+		return &metaJob{
+			Id:   id,
+			Dir:  d,
+			User: user,
+			Name: name,
+		}, nil
+	}
+}
+
+func (s *SubmissionServer) genFileName(prefix, ext string) string {
+	s.Lock()
+	defer s.Unlock()
+
+	nanos := time.Now().UnixNano()
+	return path.Join(s.JobDir, fmt.Sprintf("%v_%v.%v", prefix, nanos, ext))
+}
+
+type BeanstalkJob struct {
+	MetaId int64  `json:"meta_id"`
+	Index  int    `json:"index"`
+	User   string `json:"user"`
+	Name   string `json:"name"`
+}
+
+// Split a full job into individual jobs and queue on the beanstalk server
+func (s *SubmissionServer) QueueJob(job *metaJob) (int, error) {
+	confFile := path.Join(job.Dir, "conf.yaml")
+	conf, err := phonelab.RunnerConfFromFile(confFile)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	// Split into sources
+	splitConfs, err := conf.ShallowSplit()
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	// Persist output
+	prefix := confFile[0 : len(confFile)-5]
+	count := 0
+
+	for _, conf := range splitConfs {
+		count += 1
+		outFile := fmt.Sprintf("%v_%v.yaml", prefix, count)
+
+		if bytes, err := yaml.Marshal(conf); err != nil {
+			return http.StatusInternalServerError, fmt.Errorf("Error marhaling Yaml: %v", err)
+		} else if err = ioutil.WriteFile(outFile, bytes, 0644); err != nil {
+			return http.StatusInternalServerError, fmt.Errorf("Error writing file: %v", err)
+		}
+	}
+
+	// Set up the experiment tube. We only need to separate for the UI.
+	tubeName := job.tubeName()
+	logger.Printf("Adding %v jobs to tube %v\n", count, tubeName)
+	tube := &beanstalk.Tube{s.conn, tubeName}
+
+	// Add all individual jobs to beanstalk
+	for i := 0; i < count; i++ {
+		newJob := &BeanstalkJob{
+			MetaId: job.Id,
+			Index:  i + 1,
+			User:   job.User,
+			Name:   job.Name,
+		}
+
+		if bytes, err := json.Marshal(newJob); err != nil {
+			return http.StatusInternalServerError, fmt.Errorf("Json Marshal Error: %v", err)
+		} else if _, err = tube.Put(bytes, 1, 0, time.Second*60*60); err != nil {
+			return http.StatusInternalServerError, fmt.Errorf("Beanstalk Queue Error: %v", err)
+		}
+	}
+
+	return http.StatusOK, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// API
+
+const (
+	JsonHeader = "application/json; charset=UTF-8"
+	TextHeader = "text/plain; charset=UTF-8"
+)
+
+func sendErrorCode(w http.ResponseWriter, code int, err error) {
+	w.Header().Set("Content-Type", TextHeader)
+	w.WriteHeader(code)
+	fmt.Fprintf(w, "%v", err)
+}
+
+const (
+	ConfFileName   = "conf.yaml"
+	PluginFileName = "plugin.so"
+)
+
+// POST /submit
+func routeSubmit(w http.ResponseWriter, r *http.Request) {
+
+	metaDetails := struct {
+		User string `json:"user"`
+		Name string `json:"name"`
+	}{"", ""}
+
+	if err := r.ParseMultipartForm(100 * 1024 * 1024); err != nil {
+		logger.Printf("Parse error. Status: %v, Error: %v\n", http.StatusBadRequest, err)
+		sendErrorCode(w, http.StatusBadRequest, errors.New("Parse error"))
+		return
+	}
+
+	// Get the meta-details
+	if data, ok := r.MultipartForm.Value["data"]; ok {
+		json.Unmarshal([]byte(data[0]), &metaDetails)
+	}
+	if len(metaDetails.User) == 0 || len(metaDetails.Name) == 0 {
+		sendErrorCode(w, http.StatusBadRequest, errors.New("Expected user and experiment names"))
+		return
+	}
+
+	logger.Printf("User: %v, Name: %v\n", metaDetails.User, metaDetails.Name)
+
+	job, err := server.prepareJob(metaDetails.User, metaDetails.Name)
+	if err != nil {
+		logger.Printf("Prepare error. Status: %v, Error: %v\n", http.StatusInternalServerError, err)
+		sendErrorCode(w, http.StatusInternalServerError, errors.New("Download error"))
+		return
+	}
+
+	confFile := path.Join(job.Dir, ConfFileName)
+	pluginFile := path.Join(job.Dir, PluginFileName)
+
+	if status, err := downloadFile("conf", confFile, r); err != nil {
+		logger.Printf("Download error. Status: %v, Error: %v\n", status, err)
+		sendErrorCode(w, status, errors.New("Download error"))
+		return
+	}
+
+	if status, err := downloadFile("plugin", pluginFile, r); err != nil {
+		logger.Printf("Download error. Status: %v, Error: %v\n", status, err)
+		sendErrorCode(w, status, errors.New("Download error"))
+		return
+	}
+
+	if status, err := server.QueueJob(job); err != nil {
+		logger.Printf("Download error. Status: %v, Error: %v\n", status, err)
+		sendErrorCode(w, status, err)
+		return
+	}
+
+	// Send success, with a message
+	sendErrorCode(w, http.StatusOK,
+		fmt.Errorf("Success, your experiment has been queued in the tube '%v'", job.tubeName()))
+}
+
+// GET /conf{id}/{index}
+func routeConf(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Println(vars)
+	fmt.Println(r.URL)
+	w.WriteHeader(http.StatusOK)
+}
+
+// GET /plugin/{id}
+func routePlugin(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Println(vars)
+	fmt.Println(r.URL)
+	w.WriteHeader(http.StatusOK)
+}
+
+// Download an individual file
+func downloadFile(key, outFileName string, r *http.Request) (int, error) {
+
+	if file, _, err := r.FormFile(key); err != nil {
+		return http.StatusBadRequest, err
+	} else {
+		defer file.Close()
+
+		if f, err := os.OpenFile(outFileName, os.O_WRONLY|os.O_CREATE, 0744); err != nil {
+			return http.StatusInternalServerError, err
+		} else {
+
+			defer f.Close()
+
+			if _, err = io.Copy(f, file); err != nil {
+				return http.StatusInternalServerError, err
+			}
+
+			return http.StatusOK, nil
+		}
+	}
+}
